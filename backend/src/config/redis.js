fe8c5a7cecf75ab133b8f6f7fa4cd46320870711
zaki9254 <sharfzaki@gmail.com>
2026-03-24 12:24:52 +0530

    Removed backend folder from repository

--- backend/src/config/redis.js
+++
@@ -1,8 +0,0 @@
-const Redis = require("ioredis");
-
-const redis = new Redis(process.env.REDIS_URL);
-
-redis.on("connect", () => console.log("Redis connected"));
-redis.on("error", (err) => console.error("Redis error:", err));
-
-module.exports = redis;
